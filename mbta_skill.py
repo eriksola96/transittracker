08a8c8a8fda4fec126f7665be8455dd1bfa41cc9
eriksola96 <eriksola1996@gmail.com>
2018-11-07 21:38:26 -0500

    Added mulitple alerts support, need to filter out unwanted alerts and give them contect.

--- mbta_skill.py
+++ mbta_skill.py
@@ -9,7 +9,7 @@ from flask_ask import Ask, statement
 
 MBTAENDPOINT = 'https://api-v3.mbta.com/{}'
 KEY = { 'api_key' : config.api_key }
-human_readable=yaml.load(open('config/human_readable.yml'))
+translate = yaml.load(open('config/translations.yml'))
 
 app = Flask(__name__)
 ask = Ask(app, '/')
@@ -23,15 +23,28 @@ def launch():
 @ask.intent('GetAlertForColorIntent', convert={'color': 'color'})
 def get_alerts(color):
     alerts_data = alerts_present(color)
-    if len(alerts_data) > 0:
+    if len(alerts_data) == 1:
         cause = alerts_data[0]['attributes']['cause']
         effect = alerts_data[0]['attributes']['effect']
-        translate_effect = human_readable[effect]
-        speech_output = "Yup, there is currently an alert for %s and it's gonna cause a %s." % (cause, translate_effect)
+        speech_output = "Yup, there is currently an alert for %s and it's gonna cause %s." % \
+                        (translate[cause], translate[effect])
+        return statement('<speak>{}</speak>'.format(speech_output))
+    elif len(alerts_data) > 1:
+        speech_output = parse_multiple_alerts(alerts_data)
         return statement('<speak>{}</speak>'.format(speech_output))
     else:
         speech_output = "Nope, you're in the clear"
         return statement('<speak>{}</speak>'.format(speech_output))
+
+
+def parse_multiple_alerts(alerts):
+    speech_output = "Yeah, there's a few. "
+    for alert in alerts:
+        cause = alert['attributes']['cause']
+        effect = alert['attributes']['effect']
+        cause_and_effect = " Alert for %s and it's gonna cause %s." % (translate[cause], translate[effect])
+        speech_output += cause_and_effect
+    return speech_output
 
 
 def alerts_present(color):
